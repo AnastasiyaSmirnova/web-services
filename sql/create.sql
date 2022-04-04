c061cf015ad65f77afe174c2e5081f2209f1302c
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-04-04 11:47:48 +0300

    create sql scripts

---
+++ sql/create.sql
@@ -0,0 +1,10 @@
+create table book
+(
+    id               serial PRIMARY KEY,
+    title            varchar(30),
+    author           varchar(50),
+    publishing_house varchar(20),
+    language         varchar(10),
+    pages            integer
+);
+
---
+++ sql/insert.sql
@@ -0,0 +1,11 @@
+insert into book(title, author,  publishing_house, language, pages)
+values ('Война и мир', 'Лев Николаевич Толстой', 'Русский вестник', 'russian', 1300);
+
+insert into book(title, author,  publishing_house, language, pages)
+values ('Преступление и наказание', 'Федор Михайлович Достоевский',  'Наука', 'russian', 608);
+
+insert into book(title, author,  publishing_house, language, pages)
+values ('Евгений Онегин', 'Александр Сергеевич Пушкин', 'Азбука', 'russian', 400);
+
+insert into book(title, author,  publishing_house, language, pages)
+values ('And then there were none', 'Agata Kristi','Harper', 'english',258);
