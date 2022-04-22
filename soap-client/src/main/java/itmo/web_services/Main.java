9be38f55dfd1f263345ff2cc23f72a323d100d5e
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-04-22 11:34:37 +0300

    without wsimport test

--- soap-client/src/main/java/itmo/web_services/Main.java
+++ soap-client/src/main/java/itmo/web_services/Main.java
@@ -1,13 +1,24 @@
 package itmo.web_services;
 
+import itmo.web_services.service.BooksWebService;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         System.out.println("Client started");
 
-        String url = "http://localhost:8080/BookService?wsdl"; // standalone client
-//        String url = "http://desktop-kmrspqh:8080/books/BookService?wsdl"; // j2ee client
-        new WebClient(url);
+        URL url = new URL("http://localhost:8080/BookService?wsdl");
+        QName qName = new QName("http://service.web_services.itmo/", "BookService");
+        Service service = Service.create(url, qName);
 
+        QName port = new QName("http://service.web_services.itmo/", "BooksWebServicePort");
+        BooksWebService bookService = service.getPort(port, BooksWebService.class);
+
+        System.out.println(bookService.getImageAsString());
         System.out.println("Client finished");
     }
 }
--- soap-client/src/main/java/itmo/web_services/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.web_services.itmo/")
-package itmo.web_services.service;
